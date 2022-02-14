86dfcb93262d9d4b5079cfc6fc495020338f4a0e
Dmitriy_Yavorskiy <dyav2017@gmail.com>
2022-02-14 22:45:51 +0200

    example 1

---
+++ src/main/java/home/dhert/DhertApplication.java
@@ -0,0 +1,13 @@
+package home.dhert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhertApplication.class, args);
+	}
+
+}
